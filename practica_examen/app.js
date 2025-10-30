9a030ce26642dbe32b565d7ded883a32f7e04f23
gabrieljmnz <angelgabrielvargas313@gmail.com>
2025-10-30 14:27:35 -0600

    cotidiano

---
+++ practica_examen/app.js
@@ -0,0 +1,15 @@
+function producto(){
+
+    constructor(nombre, precio); {
+        this.nombre = nombre;
+        this.precio = precio;
+
+    }
+}
+
+class ItemFactura(){
+    constructor(producto, cantidad); {
+
+    }
+
+}
